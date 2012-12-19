cf47ed63307c3933cb0d04cdff2fb4da6ecf564d
Fabien Cadet <cadet@balios.cines.fr>
2012-12-19 10:42:38 +0100

    [fixme:] custom/ : Added 99_sybase_env.bash  (Sybase ASE RDBMS)

---
+++ custom/99_sybase_env.bash
@@ -0,0 +1,21 @@
+# F.2011-08-24 : Sybase ASE stuff.
+#
+# See /opt/sybase/SYBASE.sh
+#
+
+export SYBASE=/opt/sybase
+export SYBASE_OCS=OCS-15_0
+
+# Hummm what was that thing about ?
+#export SYBPLATFORM=linux
+
+# PATH
+[ -d "$SYBASE/$SYBASE_OCS/bin" ] && pathprepend "$SYBASE/$SYBASE_OCS/bin"
+
+# LD_LIBRARY_PATH & LIB
+[ -d "$SYBASE/$SYBASE_OCS/lib" ] \
+    && pathprepend "$SYBASE/$SYBASE_OCS/lib" LD_LIBRARY_PATH \
+    && pathprepend "$SYBASE/$SYBASE_OCS/lib" LIB
+
+# INCLUDE
+[ -d "$SYBASE/$SYBASE_OCS/include" ] && pathprepend "$SYBASE/$SYBASE_OCS/include" INCLUDE
