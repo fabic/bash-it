f581aee9714e6a36d48c0d3416e02c5a3bcbfd40
Fabien Cadet <fabi@localhost.localdomain>
2017-10-26 10:28:49 +0400

    FABIC: sublime_text.sh, bumped submodule.

---
+++ fabic/bin/sublime_text.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# FABIC/2017
+
+ST_BIN="$( type -P sublime_text )"
+
+# Not found in path.
+if [ -z "$ST_BIN" ]; then
+        # Search under /opt
+	if [ -x /opt/sublime_text/sublime_text ]; then
+                ST_BIN=/opt/sublime_text/sublime_text
+        fi
+fi
+
+# Check if binary found.
+if [ -z "$ST_BIN" ]; then
+        echo -e "\n\nERROR: Dude, I'm '${BASH_SOURCE[0]}' here talking,"
+        echo -e "         and well, I did my best, which ain't much admittedly,"
+        echo -e "         so I'll try to go straight to the point here :"
+        echo
+        echo -e "         You see, I could find the executable file for Sublime Text."
+        echo
+        echo -e "         Exiting now."
+        echo
+        echo -e "         Homepage is : https://www.sublimetext.com/"
+        echo
+        exit 127
+fi
+
+exec "$ST_BIN" "$@"
+
