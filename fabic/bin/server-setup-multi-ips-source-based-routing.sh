afd56499fe6efd650ac3296f479a9142177e101f
fabi <fabi@vps449827.ovh.net>
2018-04-01 17:59:37 +0200

    {F} pubis, done with multi-ips per one NIC source address randomization per means of SNAT

--- fabic/bin/server-setup-multi-ips-source-based-routing.sh
+++ fabic/bin/server-setup-multi-ips-source-based-routing.sh
@@ -5,7 +5,7 @@
 
 if [ $# -lt 1 ]; then
   echo
-  echo "USAGE: (sudo)  $0 <dump_tables|issue_tests|setup_output_mangling|setup_routing>"
+  echo "USAGE: (sudo)  $0 <clear_netfilter_counters|dump_tables|issue_tests|setup_output_mangling|setup_routing|setup_randomized_basic_source_routing|setup_snat>"
   echo
   echo "  TL;DR: Dude has 3 IPs on its OVH VPS box, and wants that the source address of new"
   echo "         connections (outbound packets) be set automatically to one of these (not just the default first IP of the interface)."
@@ -48,6 +48,7 @@ function dump_tables() {
     "iptables-save -c"
     "ip addr list"
     "ip rule list"
+    "ip route show cache"
     "ip route list"
     "ip route list table 92"
     "ip route list table 178"
@@ -65,6 +66,13 @@ function dump_tables() {
   done
 }
 
+function clear_netfilter_counters() {
+  iptables -t raw -Z &&
+  iptables -t mangle -Z &&
+  iptables -t nat -Z &&
+  iptables -t filter -Z &&
+  echo "~~> Cleared netfilter counters."
+}
 
 # Set up netfilter rules for marking "new" outbound packets.
 # (see setup_routing() for the matching `ip rule add fwmark ...` commands.
@@ -75,26 +83,44 @@ function setup_output_mangling() {
   echo "~~> Setting up mangle/PREROUTING (flushing first!)"
 
   # Zero packets stats counters.
-  true && (iptables -t mangle -Z && iptables -t filter -Z ) &&
+  # ^ now done after the below stuff.
+  #true && (iptables -t mangle -Z && iptables -t filter -Z ) &&
 
   iptables -t mangle -F OUTPUT &&
     # Using NF MARK target :
     # iptables -t mangle -A OUTPUT -m conntrack --ctstate NEW -m statistic --mode random --probability 0.333 -m mark --mark 0x00 -j MARK --set-mark 1 &&
     # iptables -t mangle -A OUTPUT -m conntrack --ctstate NEW -m statistic --mode random --probability 0.666 -m mark --mark 0x00 -j MARK --set-mark 2 &&
+
+    # Using CONNMARK target (as per discussion with pekster @ Freenode/#Netfilter):
     # TODO: (or maybe not?): iptables -t mangle -A OUTPUT -j CONNMARK --save-mark &&
-    iptables -t mangle -A OUTPUT -m conntrack --ctstate NEW -m statistic --mode random --probability 0.333 -m connmark --mark 0x00 -j CONNMARK --set-mark 1 &&
-    iptables -t mangle -A OUTPUT -m conntrack --ctstate NEW -m statistic --mode random --probability 0.333 -m connmark --mark 0x00 -j CONNMARK --set-mark 2 &&
+    iptables -t mangle -A OUTPUT -m conntrack --ctstate NEW -m connmark --mark 0x00/0x30 -m statistic --mode random --probability 0.333 -j CONNMARK --set-mark 0x10/0x30 &&
+    iptables -t mangle -A OUTPUT -m conntrack --ctstate NEW -m connmark --mark 0x00/0x30 -m statistic --mode random --probability 0.500 -j CONNMARK --set-mark 0x20/0x30 &&
     # As per http://ipset.netfilter.org/iptables-extensions.man.html#lbCS :
     # "Copy the ctmark to the nfmark. If a mask is specified, only those bits are copied."
-    iptables -t mangle -A OUTPUT -m connmark \! --mark 0x00 -j CONNMARK --restore-mark &&
+    iptables -t mangle -A OUTPUT -m conntrack --ctstate NEW -m connmark \! --mark 0x00/0x30 -j CONNMARK --restore-mark --ctmask 0x30 &&
 
     # DEBUG: Optional rules in the "normal (filter)" OUTPUT chain for checking if packet marking... does mark packets.
     if true; then
       iptables -t filter -F OUTPUT &&
-      iptables -t filter -A OUTPUT -m mark --mark 0x01 -j ACCEPT &&
-      iptables -t filter -A OUTPUT -m mark --mark 0x02 -j ACCEPT &&
+      iptables -t filter -A OUTPUT -m mark --mark 0x10 -j ACCEPT && # no mask here: we want the exact value as will be seen by `ip rule fwmark ...`
+      iptables -t filter -A OUTPUT -m mark --mark 0x20 -j ACCEPT &&
+      iptables -t filter -A OUTPUT -m mark \! --mark 0x00 -j ACCEPT && # Just to identify marked packets that were not previously matched.
+
       # 3 rules for matching one of our 3 IPv4 local addresses :
       if true; then
+        # Useless, indeed shows that no locally generated traffic goes through mangle/PREROUTING:
+        # iptables -t mangle -I PREROUTING 1 -m conntrack --ctstate NEW -s 91.134.136.248 -j RATEEST --rateest-name noop --rateest-interval 500ms --rateest-ewmalog 1s &&
+        # iptables -t mangle -I PREROUTING 2 -m conntrack --ctstate NEW -s 178.32.40.98   -j RATEEST --rateest-name noop --rateest-interval 500ms --rateest-ewmalog 1s &&
+        # iptables -t mangle -I PREROUTING 3 -m conntrack --ctstate NEW -s 92.222.48.73   -j RATEEST --rateest-name noop --rateest-interval 500ms --rateest-ewmalog 1s &&
+
+        # NOOP rules for matching "new connections" packets given their source address;
+        # -> proved that new connections already had their source address set when entering the nat/OUTPUT chain,
+        #    hence the illusion of having packet marking be used by `ip rule fwmark ...` to perfom "source routing" -_-
+        iptables -t mangle -I OUTPUT 1 -m conntrack --ctstate NEW -s 91.134.136.248 -j RATEEST --rateest-name noop --rateest-interval 500ms --rateest-ewmalog 1s &&
+        iptables -t mangle -I OUTPUT 2 -m conntrack --ctstate NEW -s 178.32.40.98   -j RATEEST --rateest-name noop --rateest-interval 500ms --rateest-ewmalog 1s &&
+        iptables -t mangle -I OUTPUT 3 -m conntrack --ctstate NEW -s 92.222.48.73   -j RATEEST --rateest-name noop --rateest-interval 500ms --rateest-ewmalog 1s &&
+
+        # Just to monitor traffic leaving tha box with the given source addresses.
         iptables -t filter -I OUTPUT 1 -s 91.134.136.248 -j RATEEST --rateest-name noop --rateest-interval 500ms --rateest-ewmalog 1s &&
         iptables -t filter -I OUTPUT 2 -s 178.32.40.98   -j RATEEST --rateest-name noop --rateest-interval 500ms --rateest-ewmalog 1s &&
         iptables -t filter -I OUTPUT 3 -s 92.222.48.73   -j RATEEST --rateest-name noop --rateest-interval 500ms --rateest-ewmalog 1s
@@ -106,35 +132,57 @@ function setup_output_mangling() {
     echo "OUPS! Something went wrong with your iptables ... commands (bad exit status)"
   else
     echo "~~> done adding netfilter rules to the OUTPUT/mangle"
+    clear_netfilter_counters
   fi
 
   return $retv
 }
 
+
+# Final solution (fallback)...
+function setup_snat() {
+  iptables -t nat -F POSTROUTING &&
+  iptables -t nat -Z &&
+  iptables -t nat -A POSTROUTING -o ens3 -m connmark --mark 0x10/0x30 -j SNAT --to 92.222.48.73 &&
+  iptables -t nat -A POSTROUTING -o ens3 -m connmark --mark 0x20/0x30 -j SNAT --to 178.32.40.98 &&
+  echo "\` done setting up SNAT based on previously marked packets."
+}
+
+
 # Basic "source address selection" found at
 # https://unix.stackexchange.com/a/111423
 function setup_basic_source_routing() {
-  cmds=( ip route add  32.0.0.0/3 via 91.134.136.1 dev ens3 src 178.32.40.98
-         ip route add  64.0.0.0/3 via 91.134.136.1 dev ens3 src 92.222.48.73
-         ip route add 128.0.0.0/3 via 91.134.136.1 dev ens3 src 91.134.136.248 )
+  ip route add  32.0.0.0/3 via 91.134.136.1 dev ens3 src 178.32.40.98
+  ip route add  64.0.0.0/3 via 91.134.136.1 dev ens3 src 92.222.48.73
+  ip route add 128.0.0.0/3 via 91.134.136.1 dev ens3 src 91.134.136.248
 }
+
+
+function setup_randomized_basic_source_routing() {
+  local ips=( $(shuf -e 178.32.40.98 92.222.48.73 91.134.136.248) )
+
+  echo "~~> Replacing routes with those randomized source IPs: ${ips[@]}" &&
+  ip route replace  32.0.0.0/3 via 91.134.136.1 dev ens3 src "${ips[0]}" &&
+  ip route replace  64.0.0.0/3 via 91.134.136.1 dev ens3 src "${ips[1]}" &&
+  ip route replace 128.0.0.0/3 via 91.134.136.1 dev ens3 src "${ips[2]}" &&
+  ip route flush cache &&
+  echo "\` done." &&
+
+  echo "~~> \`ip route list\`"
+  ip route list
+}
+
 
 # Sets up two routing tables for "source routing" those 2 additional "aliased" IPs we have;
 # and set up two "routing policy" rules for selecting one of these tables _based on_ the
 # fwmark of packets, supposedly.
 function setup_routing() {
   ip route flush table 92 &&
-  # ip route add default via 91.134.136.1 src 92.222.48.73 table 92  &&
-  ( ip route add  32.0.0.0/3 via 91.134.136.1 src 92.222.48.73 table 92 &&
-    ip route add  64.0.0.0/3 via 91.134.136.1 src 92.222.48.73 table 92 &&
-    ip route add 128.0.0.0/3 via 91.134.136.1 src 92.222.48.73 table 92 ) && 
+  ip route add default via 91.134.136.1 src 92.222.48.73 table 92  &&
   echo "\` done setup of routing table 92" &&
 
   ip route flush table 178 &&
-  # ip route add default via 91.134.136.1 src 178.32.40.98 table 178 &&
-  ( ip route add  32.0.0.0/3 via 91.134.136.1 src 178.32.40.98 table 178 &&
-    ip route add  64.0.0.0/3 via 91.134.136.1 src 178.32.40.98 table 178 &&
-    ip route add 128.0.0.0/3 via 91.134.136.1 src 178.32.40.98 table 178 ) && 
+  ip route add default via 91.134.136.1 src 178.32.40.98 table 178 &&
   echo "\` done setup of routing table 178" &&
 
   # TODO: investigate this statement, eventually :
@@ -143,13 +191,13 @@ function setup_routing() {
   #        ^ http://linux-ip.net/html/adv-rpdb.html
   # From `man ip-rule`: "Each rule should have an explicitly set unique priority value".
   # Blind removal of those two rules of ours.
-  ( ip rule del from all fwmark 1 priority 2 lookup  92 ;
-    ip rule del from all fwmark 2 priority 2 lookup 178 ; true ) 2>/dev/null &&
+  ( ip rule del from all fwmark 0x10 priority 1 lookup  92 ;
+    ip rule del from all fwmark 0x20 priority 2 lookup 178 ; true ) 2>/dev/null &&
       echo "\` done \"blind\" deletion of our two \"ip rules fwmark\" (about to re-add these)."
 
   echo "~~> Adding ip policy rules for selection of routing tables 92 & 178." &&
-  ip rule add fwmark 1 priority 1 table  92 &&
-  ip rule add fwmark 2 priority 2 table 178 &&
+  ip rule add fwmark 0x10 priority 1 table  92 &&
+  ip rule add fwmark 0x20 priority 2 table 178 &&
     echo "    \` done adding 2 ip rules." &&
 
   # http://linux-ip.net/html/routing-cache.html
